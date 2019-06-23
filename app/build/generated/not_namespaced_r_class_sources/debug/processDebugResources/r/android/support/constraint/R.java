8fd162f1eb0933fdea350faa9841a630ce3b4115
huangxpGitHub <522491378@qq.com>
2019-06-23 14:19:37 +0800

    地图

--- app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/constraint/R.java
+++ app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/constraint/R.java
@@ -74,19 +74,19 @@ public final class R {
         private id() {}
 
         public static final int bottom = 0x7f080024;
-        public static final int end = 0x7f080045;
-        public static final int gone = 0x7f080056;
-        public static final int invisible = 0x7f080065;
-        public static final int left = 0x7f080077;
-        public static final int packed = 0x7f08009c;
-        public static final int parent = 0x7f08009e;
-        public static final int percent = 0x7f0800a1;
-        public static final int right = 0x7f0800b1;
-        public static final int spread = 0x7f0800df;
-        public static final int spread_inside = 0x7f0800e0;
-        public static final int start = 0x7f0800e7;
-        public static final int top = 0x7f0800ff;
-        public static final int wrap = 0x7f080135;
+        public static final int end = 0x7f080046;
+        public static final int gone = 0x7f080057;
+        public static final int invisible = 0x7f080066;
+        public static final int left = 0x7f080078;
+        public static final int packed = 0x7f08009e;
+        public static final int parent = 0x7f0800a0;
+        public static final int percent = 0x7f0800a3;
+        public static final int right = 0x7f0800b3;
+        public static final int spread = 0x7f0800e1;
+        public static final int spread_inside = 0x7f0800e2;
+        public static final int start = 0x7f0800e9;
+        public static final int top = 0x7f080101;
+        public static final int wrap = 0x7f080137;
     }
     public static final class styleable {
         private styleable() {}
--- app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/fragment/R.java
+++ app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/fragment/R.java
@@ -95,33 +95,33 @@ public final class R {
         public static final int async = 0x7f08001e;
         public static final int blocking = 0x7f080022;
         public static final int bottom = 0x7f080024;
-        public static final int chronometer = 0x7f08002c;
-        public static final int end = 0x7f080045;
-        public static final int forever = 0x7f080053;
-        public static final int icon = 0x7f08005d;
-        public static final int icon_group = 0x7f08005e;
-        public static final int info = 0x7f080064;
-        public static final int italic = 0x7f080066;
-        public static final int left = 0x7f080077;
-        public static final int line1 = 0x7f080079;
-        public static final int line3 = 0x7f08007a;
-        public static final int none = 0x7f080096;
-        public static final int normal = 0x7f080097;
-        public static final int notification_background = 0x7f080098;
-        public static final int notification_main_column = 0x7f080099;
-        public static final int notification_main_column_container = 0x7f08009a;
-        public static final int right = 0x7f0800b1;
-        public static final int right_icon = 0x7f0800b2;
-        public static final int right_side = 0x7f0800b3;
-        public static final int start = 0x7f0800e7;
-        public static final int tag_transition_group = 0x7f0800ed;
-        public static final int tag_unhandled_key_event_manager = 0x7f0800ee;
-        public static final int tag_unhandled_key_listeners = 0x7f0800ef;
-        public static final int text = 0x7f0800f0;
-        public static final int text2 = 0x7f0800f1;
-        public static final int time = 0x7f0800f9;
-        public static final int title = 0x7f0800fa;
-        public static final int top = 0x7f0800ff;
+        public static final int chronometer = 0x7f08002d;
+        public static final int end = 0x7f080046;
+        public static final int forever = 0x7f080054;
+        public static final int icon = 0x7f08005e;
+        public static final int icon_group = 0x7f08005f;
+        public static final int info = 0x7f080065;
+        public static final int italic = 0x7f080067;
+        public static final int left = 0x7f080078;
+        public static final int line1 = 0x7f08007a;
+        public static final int line3 = 0x7f08007b;
+        public static final int none = 0x7f080098;
+        public static final int normal = 0x7f080099;
+        public static final int notification_background = 0x7f08009a;
+        public static final int notification_main_column = 0x7f08009b;
+        public static final int notification_main_column_container = 0x7f08009c;
+        public static final int right = 0x7f0800b3;
+        public static final int right_icon = 0x7f0800b4;
+        public static final int right_side = 0x7f0800b5;
+        public static final int start = 0x7f0800e9;
+        public static final int tag_transition_group = 0x7f0800ef;
+        public static final int tag_unhandled_key_event_manager = 0x7f0800f0;
+        public static final int tag_unhandled_key_listeners = 0x7f0800f1;
+        public static final int text = 0x7f0800f2;
+        public static final int text2 = 0x7f0800f3;
+        public static final int time = 0x7f0800fb;
+        public static final int title = 0x7f0800fc;
+        public static final int top = 0x7f080101;
     }
     public static final class integer {
         private integer() {}
@@ -131,17 +131,17 @@ public final class R {
     public static final class layout {
         private layout() {}
 
-        public static final int notification_action = 0x7f0b0037;
-        public static final int notification_action_tombstone = 0x7f0b0038;
-        public static final int notification_template_custom_big = 0x7f0b0039;
-        public static final int notification_template_icon_group = 0x7f0b003a;
-        public static final int notification_template_part_chronometer = 0x7f0b003b;
-        public static final int notification_template_part_time = 0x7f0b003c;
+        public static final int notification_action = 0x7f0b0038;
+        public static final int notification_action_tombstone = 0x7f0b0039;
+        public static final int notification_template_custom_big = 0x7f0b003a;
+        public static final int notification_template_icon_group = 0x7f0b003b;
+        public static final int notification_template_part_chronometer = 0x7f0b003c;
+        public static final int notification_template_part_time = 0x7f0b003d;
     }
     public static final class string {
         private string() {}
 
-        public static final int status_bar_notification_info_overflow = 0x7f0f0046;
+        public static final int status_bar_notification_info_overflow = 0x7f0f004e;
     }
     public static final class style {
         private style() {}
